900e98de4181b93f365ce44c1136550753aa4bb5
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-10-07 20:40:37 +0100

    1:09:07

---
+++ src/_asset/JS/color.js
@@ -0,0 +1,21 @@
+/*
+var colorList = {
+    'cascade' : {
+        colorNormal: '#96a5a6',
+        colorDark: '#7f8c8d'
+    },
+    'zest' : {
+        colorNormal: '#e67e22',
+        colorDark: '#d35400'
+    },
+    'buttercup' : {
+        colorNormal: '#f1c40f',
+        colorDark: '#f39c12'
+    }
+};
+
+console.log(colorList);
+*/
+
+const themeButtonList = [...document.querySelectorAll('.theme-button')];
+themeButtonList.map(themeButton => console.log(themeButton));
